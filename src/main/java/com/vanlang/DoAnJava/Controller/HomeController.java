9b71f97fa16a0c1421c2b6a55b833fc2ef277074
CHI TUAN <buituan110404@gmail.com>
2024-07-16 22:41:47 +0700

    init project

--- src/main/java/com/vanlang/DoAnJava/Controller/HomeController.java
+++ src/main/java/com/vanlang/DoAnJava/Controller/HomeController.java
@@ -3,11 +3,59 @@ package com.vanlang.DoAnJava.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-    @GetMapping("/home")
-    public String homepage(Model model) {
-        return "HTML/home.html";
+
+    @GetMapping
+    public String homePage(Model model) {
+        return "HTML/home";
+    }
+
+    @GetMapping("/giohang")
+    public String giohang() {
+        return "HTML/giohang";
+    }
+
+    @GetMapping("/dong-ho")
+    public String dongho() {
+        return "HTML/dong-ho";
+    }
+
+    @GetMapping("/casio")
+    public String casio() {
+        return "HTML/casio";
+    }
+
+    @GetMapping("/rolex")
+    public String rolex() {
+        return "HTML/rolex";
+    }
+
+    @GetMapping("/phukien")
+    public String phukien() {
+        return "HTML/phukien";
+    }
+
+    @GetMapping("/thongtin")
+    public String thongtin() {
+        return "HTML/thongtin";
+    }
+
+    @GetMapping("/dichvu")
+    public String dichvu() {
+        return "HTML/dichvu";
+    }
+
+    @GetMapping("/dangky")
+    public String dangky() {
+        return "HTML/dangky";
+    }
+
+    @GetMapping("/dangnhap")
+    public String dangnhap() {
+        return "HTML/dangnhap";
     }
 }
